871222949b847c1cbf84961c6fa5869f3b05a0fd
Uliana Boiko <clubfate@gmail.com>
2023-08-31 21:58:26 +0300

    JS home

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let a = 5;
+let b = 5;
+let sum = a + b;
+console.log(sum);
+
+let a1 = 5;
+let b1 = '5';
+let c1 = -10;
+let n1 = a1 + b1 - c1;
+console.log(n1);
+
+let a2 = 5;
+let b2 = 5;
+let c2 = -10;
+let n2 = a2 + b2 - c2;
+console.log(n2);
+
+let a3 = 5;
+let b3 = 63;
+let c3 = 8;
+let d3 = 4;
+let x = a3 * (b3 - c3) / d3;
+console.log(x);
+
+let a4 = 5;
+let b4 = 4;
+let c4 = 7;
+let d4 = 2;
+let e4 = 15;
+let f4 = 3;
+let y = a4 * (b4 * c4 - d4) / ((e4 * f4) - f4);
+let p = a4 * (b4 * c4 - d4) / (e4 * f4) - f4;
+console.log(y);
+console.log(p);
+
+let k = 5;
+let m = 7;
+let s = k + m;
+console.log(s);
+k = m;
+s = k + m;
+console.log(s);
+m = 15;
+s = k + m;
+console.log('Число S=' + s);
+
+//ділення на 0, цікава відповідь у консолі
+let l = 25;
+let o = 0;
+let q = l / o;
+console.log(q);
+q = o / l;
+console.log(q);
